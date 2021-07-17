a4b6b43ff9b5325665330ecc09f88996b9b3b913
Harsh Indoria <68156841+Indoriajs@users.noreply.github.com>
2021-07-17 08:53:51 +0530

    Delete app/src/main/java/com/example/mycalculator/misc/Docs directory

--- app/src/main/java/com/example/mycalculator/misc/Docs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"stack","c":"StackEval","l":"evalPostfix(List<String>)","u":"evalPostfix(java.util.List)"},{"p":"stack","c":"StackEval","l":"infixToPostfix()"},{"p":"stack","c":"StackEval","l":"setINFIX_EXPR(String)","u":"setINFIX_EXPR(java.lang.String)"},{"p":"stack","c":"StackEval","l":"StackEval()","u":"%3Cinit%3E()"}];updateSearchResults();
